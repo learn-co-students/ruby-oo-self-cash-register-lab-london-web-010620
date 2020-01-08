0737ce7548495f9abcae98f8348b6eea811feaa0
simon <sjtucker28@gmail.com>
2020-01-08 16:46:48 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
     def add_item(title, price, quantity = 1)
         @total += price*quantity
-        @items.fill(title, @items.length, quantity)
+        @items.fill(title, quantity)
         @void = price * quantity
     end
 
