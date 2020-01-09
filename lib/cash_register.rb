1b10ba3c79f3a5d81d8ce3002346623b6262ec26
simon <sjtucker28@gmail.com>
2020-01-09 17:30:19 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,11 +22,10 @@ class CashRegister
         if discount == 0 
             return "There is no discount to apply."
         else
-
-        @total -= total*discount / 100
-        return "After the discount, the total comes to $#{@total}."
+            @total -= total*discount / 100
+            return "After the discount, the total comes to $#{@total}."
         end
-        
+
     end
 
     def items
